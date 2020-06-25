09c3a121ca7c9f7a8ce55154ea63183bff5d57b1
alvesdario <dario.alves@fatec.sp.gov.br>
2020-06-24 23:20:45 -0300

    Improve aluno page

--- src/components/alunoTable.js
+++ src/components/alunoTable.js
@@ -1,40 +1,54 @@
 import React, { useState } from 'react';
-import './alunoTable.css'
+import './searchTable.css';
+import Axios from 'axios';
 
 const App = () => {
-    const [list, setList] = useState([{nome:"Joao", RA: 123456789, curso: "ADS"}]);
-    
-    // new function(){setList([...list, ]);}()
-    const handleTrClick = (e) => {
-        window.location.href = "/aluno/"+e;
-    }
+  const [alunoTable, setAlunoTable] = useState([]);
 
-    return (<>
-    <div className="alunoForm">
-        <div className="alunoInputForm">
-            <label>Nome do Aluno</label>
-            <input />
-        </div>
-        <div className="alunoInputForm">
-            <label>RA</label>
-            <input />
-        </div>
-        <div className="alunoInputForm">
-            <label>Curso</label>
-            <input />
-        </div>
-        <button>Pesquisar</button>
+  const [searchName, setSearchName] = useState("");
+  const [searchRA, setSearchRA] = useState("");
+  const [searchCurso, setSearchCurso] = useState("");
+
+  const handleTrClick = (e) => {
+    window.location.href += "/" + e;
+  }
+  
+  const handleSearchClick = () => {
+    Axios.get(/** pega lista de alunos*/)
+      .then(res => setAlunoTable(res));
+  }
+
+  return (<>
+    <div className="searchForm">
+      <div className="searchInputForm">
+        <label>Nome do Aluno</label>
+        <input value={searchName} onChange={e => {setSearchName(e.target.value)}}/>
+      </div>
+      <div className="searchInputForm">
+        <label>RA</label>
+        <input value={searchRA} onChange={e => {setSearchRA(e.target.value)}}/>
+      </div>
+      <div className="searchInputForm">
+        <label>Curso</label>
+        <input value={searchCurso} onChange={e => {setSearchCurso(e.target.value)}}/>
+      </div>
+      <button onClick={handleSearchClick}>Pesquisar</button>
     </div>
-        <table className="alunoTable">
-            <tbody>
-            {list.map(aluno => <tr key={aluno.RA} onClick={()=>handleTrClick(aluno.RA)}>
-                <td>{aluno.nome}</td>
-                <td>{aluno.RA}</td>
-                <td>{aluno.curso}</td>
+    
+    {alunoTable.length === 0 ? (
+      <p>Nenhum aluno foi encontrado</p>
+    ) : (
+        <table className="searchTable">
+          <tbody>
+            {alunoTable.map(aluno => <tr key={aluno.RA} onClick={() => handleTrClick(aluno.RA)}>
+              <td>{aluno.nome}</td>
+              <td>{aluno.RA}</td>
+              <td>{aluno.curso}</td>
             </tr>)}
-            </tbody>
+          </tbody>
         </table>
-    </>);
+      )}
+  </>);
 };
 
 export default App;
--- src/pages/alunos.js
+++ src/pages/alunos.js
@@ -1,10 +1,11 @@
-import { useParams} from "react-router";
-import React, { useState } from 'react';
+import { useParams } from "react-router";
+import React, { useState, useEffect } from 'react';
 import SideNav from '../components/sidenav';
 import AlunoTable from '../components/alunoTable';
+import Axios from "axios";
 
 const App = () => {
-    const {RA} = useParams();
+    const { RA } = useParams();
     const [edit, setEdit] = useState(false);
     const [nome, setNome] = useState("Joao");
     const [nascimento, setNascimento] = useState("20/10/1960");
@@ -17,37 +18,53 @@ const App = () => {
     const [CEP, setCEP] = useState("13150-352");
     const [telefone, setTelefone] = useState("19 3898-2357");
     const [celular, setCelular] = useState("19 98246-8342");
-    
+
+    useEffect(() => {
+        Axios.get(/** pegar dados do aluno baseado no RA*/).then(res => {
+            setNome(res.nome);
+            setNascimento(res.nascimento);
+            setNomeMae(res.nomeMae);
+            setEstadoCivil(res.estadoCivil);
+            setCurso(res.curso);
+            setTurno(res.turno);
+            setEmail(res.email);
+            setEndereco(res.endereco);
+            setCEP(res.CEP);
+            setTelefone(res.telefone);
+            setCelular(res.celular);
+        })
+    }, [RA])
+
     return (<>
-        <SideNav/>
+        <SideNav />
         <div className="content">
             <h1>Alunos</h1>
             {RA && <>
                 <label>RA:</label>
                 <input type="text" value={RA} disabled={true} />
                 <label>Nome:</label>
-                <input type="text" value={nome} disabled={!edit} onChange={(e)=>{setNome(e.target.value)}}/>
+                <input type="text" value={nome} disabled={!edit} onChange={(e) => { setNome(e.target.value) }} />
                 <label>Nascimento:</label>
-                <input type="text" value={nascimento} disabled={!edit} onChange={(e)=>{setNascimento(e.target.value)}}/>
+                <input type="text" value={nascimento} disabled={!edit} onChange={(e) => { setNascimento(e.target.value) }} />
                 <label>Nome da mãe:</label>
-                <input type="text" value={nomeMae} disabled={!edit} onChange={(e)=>{setNomeMae(e.target.value)}}/>
+                <input type="text" value={nomeMae} disabled={!edit} onChange={(e) => { setNomeMae(e.target.value) }} />
                 <label>Estado civil:</label>
-                <input type="text" value={estadoCivil} disabled={!edit} onChange={(e)=>{setEstadoCivil(e.target.value)}}/>
+                <input type="text" value={estadoCivil} disabled={!edit} onChange={(e) => { setEstadoCivil(e.target.value) }} />
                 <label>Curso:</label>
-                <input type="text" value={curso} disabled={!edit} onChange={(e)=>{setCurso(e.target.value)}}/>
+                <input type="text" value={curso} disabled={!edit} onChange={(e) => { setCurso(e.target.value) }} />
                 <label>Turno:</label>
-                <input type="text" value={turno} disabled={!edit} onChange={(e)=>{setTurno(e.target.value)}}/>
+                <input type="text" value={turno} disabled={!edit} onChange={(e) => { setTurno(e.target.value) }} />
                 <label>Email:</label>
-                <input type="text" value={email} disabled={!edit} onChange={(e)=>{setEmail(e.target.value)}}/>
+                <input type="text" value={email} disabled={!edit} onChange={(e) => { setEmail(e.target.value) }} />
                 <label>Endereco:</label>
-                <input type="text" value={endereco} disabled={!edit} onChange={(e)=>{setEndereco(e.target.value)}}/>
+                <input type="text" value={endereco} disabled={!edit} onChange={(e) => { setEndereco(e.target.value) }} />
                 <label>CEP:</label>
-                <input type="text" value={CEP} disabled={!edit} onChange={(e)=>{setCEP(e.target.value)}}/>
+                <input type="text" value={CEP} disabled={!edit} onChange={(e) => { setCEP(e.target.value) }} />
                 <label>Telefone:</label>
-                <input type="text" value={telefone} disabled={!edit} onChange={(e)=>{setTelefone(e.target.value)}}/>
+                <input type="text" value={telefone} disabled={!edit} onChange={(e) => { setTelefone(e.target.value) }} />
                 <label>Celular:</label>
-                <input type="text" value={celular} disabled={!edit} onChange={(e)=>{setCelular(e.target.value)}}/>
-                <button onClick={()=>setEdit(!edit)}>editar</button>
+                <input type="text" value={celular} disabled={!edit} onChange={(e) => { setCelular(e.target.value) }} />
+                <button onClick={() => setEdit(!edit)}>editar</button>
             </>}
             {!RA && <>
                 <AlunoTable />
