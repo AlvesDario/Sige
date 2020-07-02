da0e163be875f2f0402632b91e97a0c99fa56687
alvesdario <dario.alves@fatec.sp.gov.br>
2020-07-01 22:50:26 -0300

    Fix missing useEffect

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import SideNav from '../components/sidenav';
 import './homepage.css';
 import { Context } from '../components/Wrapper';
