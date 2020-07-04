1d3393e0010d447e1e8c55c0577da4bea2041e5b
alvesdario <dario.alves@fatec.sp.gov.br>
2020-07-04 19:45:51 -0300

    Fix aluno page bugs and search

--- src/components/alunoTable.js
+++ src/components/alunoTable.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import './searchTable.css';
 import Axios from 'axios';
+import { useEffect } from 'react';
 
 const App = () => {
   const [alunoTable, setAlunoTable] = useState([]);
@@ -10,12 +11,29 @@ const App = () => {
   const [searchRA, setSearchRA] = useState("");
   const [searchCurso, setSearchCurso] = useState("");
 
+  useEffect(() => {
+    Axios.get("https://jsonbox.io/box_828dac9abb903c98aab7/alunos")
+      .then(res => setAlunoTable(res.data));
+  }, [])
+
   const handleTrClick = (e) => {
     window.location.href += "/" + e;
   }
-  
+
   const handleSearchClick = () => {
-    Axios.get("https://jsonbox.io/box_c2aba15389ee5cfa5983/alunos")
+    if (!searchName && !searchRA && !searchCurso) {
+      Axios.get("https://jsonbox.io/box_828dac9abb903c98aab7/alunos")
+        .then(res => setAlunoTable(res.data));
+      return;
+    }
+    let query = "?q="
+    if (searchName)
+      query += "nome:" + searchName + "*";
+    if (searchRA)
+      query += (searchName ? "," : "") + "RA:" + searchRA + "*";
+    if (searchCurso)
+      query += (searchName || searchRA ? "," : "") + "curso:" + searchCurso + "*";
+    Axios.get("https://jsonbox.io/box_828dac9abb903c98aab7/alunos" + query)
       .then(res => setAlunoTable(res.data));
   }
 
@@ -23,19 +41,19 @@ const App = () => {
     <div className="searchForm">
       <div className="searchInputForm">
         <label><FormattedMessage id="nome_aluno" /></label>
-        <input value={searchName} onChange={e => {setSearchName(e.target.value)}}/>
+        <input value={searchName} onChange={e => { setSearchName(e.target.value) }} />
       </div>
       <div className="searchInputForm">
         <label>RA</label>
-        <input value={searchRA} onChange={e => {setSearchRA(e.target.value)}}/>
+        <input value={searchRA} onChange={e => { setSearchRA(e.target.value) }} />
       </div>
       <div className="searchInputForm">
         <label><FormattedMessage id="curso" /></label>
-        <input value={searchCurso} onChange={e => {setSearchCurso(e.target.value)}}/>
+        <input value={searchCurso} onChange={e => { setSearchCurso(e.target.value) }} />
       </div>
       <button onClick={handleSearchClick}><FormattedMessage id='pesquisar' /></button>
     </div>
-    
+
     {alunoTable.length === 0 ? (
       <p>Nenhum aluno foi encontrado</p>
     ) : (
--- src/pages/alunos.js
+++ src/pages/alunos.js
@@ -9,7 +9,6 @@ import Axios from "axios";
 const App = () => {
   const context = useContext(Context);
   const { RA } = useParams();
-  const [edit, setEdit] = useState(false);
   const [nome, setNome] = useState("");
   const [nascimento, setNascimento] = useState("");
   const [nomeMae, setNomeMae] = useState("");
@@ -23,71 +22,30 @@ const App = () => {
   const [celular, setCelular] = useState("");
 
   useEffect(() => {
-    if (RA)
-      if (!edit) {
-        Axios.get("https://jsonbox.io/box_c2aba15389ee5cfa5983/alunos?q=RA:" + RA).then(({ data }) => {
-          if (data[0] ? 1 : 0) {
-            Axios.put("https://jsonbox.io/box_c2aba15389ee5cfa5983/alunos/" + data[0]._id, {
-              RA: RA,
-              nome: nome,
-              nascimento: nascimento,
-              nomeMae: nomeMae,
-              estadoCivil: estadoCivil,
-              curso: curso,
-              turno: turno,
-              email: email,
-              endereco: endereco,
-              CEP: CEP,
-              telefone: telefone,
-              celular: celular
-            })
-          }
-          else {
-            Axios.post("https://jsonbox.io/box_c2aba15389ee5cfa5983/alunos", {
-              RA: RA,
-              nome: nome,
-              nascimento: nascimento,
-              nomeMae: nomeMae,
-              estadoCivil: estadoCivil,
-              curso: curso,
-              turno: turno,
-              email: email,
-              endereco: endereco,
-              CEP: CEP,
-              telefone: telefone,
-              celular: celular
-            }).then(data => {
-              console.log("cadastrado com sucesso")
-            })
-          }
-        })
-      }
-  }, [edit, CEP, RA, celular, curso, email, endereco, estadoCivil, nascimento, nome, nomeMae, telefone, turno]);
+    if (RA) {
+      console.log(RA)
+      Axios.get("https://jsonbox.io/box_828dac9abb903c98aab7/alunos?q=RA:" + RA).then(({ data }) => {
+      try{
+        const Aluno = Array.isArray(data) ? data[0] : [];
+        setNome(Aluno.nome)
+        setNascimento(Aluno.nascimento)
+        setNomeMae(Aluno.nomeMae)
+        setEstadoCivil(Aluno.estadoCivil)
+        setCurso(Aluno.curso)
+        setTurno(Aluno.turno)
+        setEmail(Aluno.email)
+        setEndereco(Aluno.endereco)
+        setCEP(Aluno.CEP)
+        setTelefone(Aluno.telefone)
+        setCelular(Aluno.celular)
+      }catch (err) {console.log(err)}
+      });
+    }
+  }, [RA]);
 
   useEffect(() => {
     context.selectLang();
-  }, [context])
-
-  useEffect(() => {
-    if (RA) {
-      Axios.get("").then(({ data }) => {
-        if (data[0] ? 1 : 0) {
-          const alunoData = data[0];
-          setNome(alunoData.nome)
-          setNascimento(alunoData.nascimento)
-          setNomeMae(alunoData.nomeMae)
-          setEstadoCivil(alunoData.estadoCivil)
-          setCurso(alunoData.curso)
-          setTurno(alunoData.turno)
-          setEmail(alunoData.email)
-          setEndereco(alunoData.endereco)
-          setCEP(alunoData.CEP)
-          setTelefone(alunoData.telefone)
-          setCelular(alunoData.celular)
-        }
-      })
-    }
-  }, [RA])
+  }, [context]);
 
   return (<>
     <SideNav />
@@ -95,30 +53,29 @@ const App = () => {
       <h1><FormattedMessage id="alunos" /></h1>
       {RA && <>
         <label>RA:</label>
-        <input type="text" value={RA} disabled={true} />
+        <input type="text" value={RA} />
         <label><FormattedMessage id="nome" /></label>
-        <input type="text" value={nome} disabled={!edit} onChange={(e) => { setNome(e.target.value) }} />
+        <input type="text" value={nome} />
         <label><FormattedMessage id="nascimento" /></label>
-        <input type="text" value={nascimento} disabled={!edit} onChange={(e) => { setNascimento(e.target.value) }} />
+        <input type="text" value={nascimento} />
         <label><FormattedMessage id="nome_mae" /></label>
-        <input type="text" value={nomeMae} disabled={!edit} onChange={(e) => { setNomeMae(e.target.value) }} />
+        <input type="text" value={nomeMae} />
         <label><FormattedMessage id="estado_civil" /></label>
-        <input type="text" value={estadoCivil} disabled={!edit} onChange={(e) => { setEstadoCivil(e.target.value) }} />
+        <input type="text" value={estadoCivil} />
         <label><FormattedMessage id="Curso" /></label>
-        <input type="text" value={curso} disabled={!edit} onChange={(e) => { setCurso(e.target.value) }} />
+        <input type="text" value={curso} />
         <label><FormattedMessage id="turno" /></label>
-        <input type="text" value={turno} disabled={!edit} onChange={(e) => { setTurno(e.target.value) }} />
+        <input type="text" value={turno} />
         <label><FormattedMessage id="e-mail" /></label>
-        <input type="text" value={email} disabled={!edit} onChange={(e) => { setEmail(e.target.value) }} />
+        <input type="text" value={email} />
         <label><FormattedMessage id="endereco" /></label>
-        <input type="text" value={endereco} disabled={!edit} onChange={(e) => { setEndereco(e.target.value) }} />
+        <input type="text" value={endereco} />
         <label><FormattedMessage id="cep" /></label>
-        <input type="text" value={CEP} disabled={!edit} onChange={(e) => { setCEP(e.target.value) }} />
+        <input type="text" value={CEP} />
         <label><FormattedMessage id="telefone" /></label>
-        <input type="text" value={telefone} disabled={!edit} onChange={(e) => { setTelefone(e.target.value) }} />
+        <input type="text" value={telefone} />
         <label><FormattedMessage id="celular" /></label>
-        <input type="text" value={celular} disabled={!edit} onChange={(e) => { setCelular(e.target.value) }} />
-        <button onClick={() => setEdit(!edit)}><FormattedMessage id="editar" /></button>
+        <input type="text" value={celular} />
       </>}
       {!RA && <>
         <AlunoTable />
