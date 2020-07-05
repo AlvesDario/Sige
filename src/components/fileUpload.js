bfa02e34a625a451bf78103ede657f14a15b3fa8
alvesdario <dario.alves@fatec.sp.gov.br>
2020-07-05 17:16:53 -0300

    Improve fileUpload feature

--- src/components/fileUpload.js
+++ src/components/fileUpload.js
@@ -3,35 +3,42 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 import { FormattedMessage } from 'react-intl';
+import { FormGroup, FormControl } from 'react-bootstrap';
 
 const App = () => {
   const [selectedFile, setSelectedFile] = useState(null);
-
-  // On file select (from the pop up) 
+  const [cursoID, setCursoID] = useState(0);
+  const [message, setMessage] = useState("");
   const onFileChange = event => {
-    // Update the state 
     setSelectedFile(event.target.files[0]);
   };
 
-  // On file upload (click the upload button) 
+
   const onFileUpload = () => {
     // Create an object of formData 
     const formData = new FormData();
-    // Update the formData object 
+    // // Update the formData object 
     formData.append(
-      "myFile",
-      selectedFile,
-      selectedFile.name
+      "file",
+      selectedFile
     );
     // Details of the uploaded file 
-    console.log(selectedFile);
+    console.log({ file: selectedFile });
+    console.log(cursoID)
+
     // Request made to the backend api 
     // Send formData object 
-    axios.post("api/uploadfile", formData);
+    // axios.post("https://45.79.139.78/v1/import_data/upload/" + cursoID, formData, {
+    //   headers: {
+    //     "Content-Type": 'multipart/form-data',
+    //     Authorization: 'Bearer ' + localStorage.getItem('jwtToken')
+    //   }
+    // }).then(({ data }) => {
+    setSelectedFile(null);
+    setMessage("this may take a while");//data.message);
+    // })
   };
 
-  // File content to be displayed after 
-  // file upload is complete 
   const fileData = () => {
     if (selectedFile) {
       return (
@@ -49,17 +56,35 @@ const App = () => {
       return (
         <div>
           <br />
-          <h4><FormattedMessage id="escolher_arquivo" /></h4>
+          <h4>
+            {message ||
+              <FormattedMessage id="escolher_arquivo" />}
+          </h4>
         </div>
       );
     }
   };
 
+  const validForm = () => {
+    return (selectedFile ? true : false) && cursoID != 0;
+  }
+
   return (
     <div>
       <div>
         <input type="file" onChange={onFileChange} />
-        <button onClick={onFileUpload}>
+        <FormGroup>
+          <FormControl as="select" onChange={e => setCursoID(e.target.value)}>
+            <option value="0">escolha um curso...</option>
+            <option value="1">Análise e Desenvolvimento de Sistemas</option>
+            <option value="2">Comércio Exterior</option>
+            <option value="3">Gestão Empresarial</option>
+            <option value="4">Logística Aeroportuária</option>
+            <option value="5">Redes de Computadores</option>
+            <option value="6">Serviços</option>
+          </FormControl>
+        </FormGroup>
+        <button disabled={!validForm()} onClick={onFileUpload}>
           <FormattedMessage id="enviar" />
         </button>
       </div>
