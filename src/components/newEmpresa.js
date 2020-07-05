562d9151d8eac66cdd6b22a71b3b1b18fe1827d7
Dario Alves <44751180+AlvesDario@users.noreply.github.com>
2020-07-05 18:15:28 -0300

    Create newEmpresa.js

---
+++ src/components/newEmpresa.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { Button, Form } from 'react-bootstrap';
+
+const App = (props) => {
+  const [CNPJ, setCNPJ] = useState("");
+  const [razao, setRazao] = useState("");
+  const [dataAbertura, setDataAbertura] = useState("");
+  const [email, setEmail] = useState("");
+  const [CEP, setCEP] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [celular, setCelular] = useState("");
+  const [contato, setContato] = useState("");
+  const [inicioConvenio, setinicioConvenio] = useState("");
+  const [fimConvenio, setFimConvenio] = useState("");
+  const [show, setShow] = React.useState(false);
+
+
+  const checkCNPJ = () => {
+    if (CNPJ) {
+      let cnpj = CNPJ.replace('.', '').replace('-', '').replace('/', '');
+      console.log(cnpj);
+      setShow(true);
+      Axios.get("https://www.receitaws.com.br/v1/cnpj/" + CNPJ).then(data => {
+        setRazao(data.nome);
+        setCEP(data.cep);
+        setEndereco(data.logradouro + ", N: " + data.numero + ", " + data.bairro + ", " + data.municipio + " - " + data.uf);
+      });
+    }
+  };
+
+  const handleAdd = () => {
+    // Axios.post("https://45.79.139.78/v1/associated_companies/companies/", {
+    //   new_company_data: {
+    //     address: endereco,
+    //     associated_since: inicioConvenio,
+    //     associated_until: fimConvenio,
+    //     cnpj: CNPJ,
+    //     company_name: razao,
+    //     contact_email: email,
+    //     contact_person: contato,
+    //     contact_phone: celular,
+    //     opening_date: dataAbertura,
+    //     zip_code: CEP
+    //   }
+    // })
+    props.onAdd([{ nome: razao, CNPJ: CNPJ, nConvenio: "2312" }])
+  }
+
+  const validAdd = () => {
+    return CNPJ.length > 0 && razao.length > 0 && dataAbertura > 0 && email.length > 0 && CEP.length > 0 && inicioConvenio.length > 0 && fimConvenio.length > 0;
+  }
+
+  return (
+    <>
+      <Form >
+        <h1>Adicionar novo convênio</h1>
+        <Form.Group>
+          <Form.Label>CNPJ:</Form.Label>
+          <Form.Control value={CNPJ} onChange={e => setCNPJ(e.target.value)} onBlur={checkCNPJ} type="text" placeholder="CNPJ" />
+
+          <Form.Text className="text-muted">
+            {}
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group hidden={!show}>
+          <Form.Label>Razão Social:</Form.Label>
+          <Form.Control value={razao} type="text" placeholder="Razão Social" onChange={e => setRazao(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group hidden={!show}>
+          <Form.Label>Data de abertura:</Form.Label>
+          <Form.Control value={dataAbertura} type="date" placeholder="Data de abertura" onChange={e => setDataAbertura(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group hidden={!show}>
+          <Form.Label>CEP:</Form.Label>
+          <Form.Control value={CEP} type="number" placeholder="CEP" onChange={e => setCEP(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group hidden={!show}>
+          <Form.Label>Endereco:</Form.Label>
+          <Form.Control value={endereco} type="text" placeholder="Endereco" onChange={e => setEndereco(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group hidden={!show}>
+          <Form.Label>Telefone:</Form.Label>
+          <Form.Control value={telefone} type="tel" placeholder="Telefone de contato" onChange={(e) => { setTelefone(e.target.value) }} />
+        </Form.Group>
+
+        <Form.Group hidden={!show}>
+          <Form.Label>Celular:</Form.Label>
+          <Form.Control value={celular} type="tel" placeholder="Celular de contato" onChange={(e) => { setCelular(e.target.value) }} />
+        </Form.Group>
+        
+        <Form.Group hidden={!show}>
+          <Form.Label>Contato:</Form.Label>
+          <Form.Control value={contato} type="text" placeholder="Nome da pessoa a contatar" onChange={(e) => { setContato(e.target.value) }} />
+        </Form.Group>
+
+        <Form.Group hidden={!show}>
+          <Form.Label>Email:</Form.Label>
+          <Form.Control value={email} type="email" placeholder="Email de contato" onChange={(e) => { setEmail(e.target.value) }} />
+        </Form.Group>
+        <Form.Group hidden={!show}>
+          <Form.Label>Inicio do Convenio:</Form.Label>
+          <Form.Control value={inicioConvenio} type="date" placeholder="Inicio do Convenio" onChange={(e) => { setinicioConvenio(e.target.value) }} />
+        </Form.Group>
+        <Form.Group hidden={!show}>
+          <Form.Label>Termino do convenio:</Form.Label>
+          <Form.Control value={fimConvenio} type="date" placeholder="Termino do convenio" onChange={(e) => { setFimConvenio(e.target.value) }} />
+        </Form.Group>
+        <Form.Group hidden={!show}>
+          <Button onClick={handleAdd} disabled={!validAdd()}>
+            Adicionar
+            </Button>
+        </Form.Group>
+      </Form>
+    </>
+  );
+}
+
+export default App;
