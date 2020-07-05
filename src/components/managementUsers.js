9cdb69fbed5ba9278930e6816430578425776ac1
Claudio Grosman <henrique.grosman@hotmail.com>
2020-07-05 16:40:19 -0300

    Access management

--- src/components/managementUsers.js
+++ src/components/managementUsers.js
@@ -9,79 +9,83 @@ const App = () => {
   const context = useContext(Context);
   const [users, setUsers] = useState([]);
 
-  const [newRole, setNewRole] = useState("");
-
   const [error, setError] = useState(0);
-
-  function teste(id, event) {
-      event.preventDefault();
-      let myResult = event.target.value;
-
-      console.log("O id da minha role: "+ myResult);
-      console.log("o id do meu user: "+ id);
-  }
 
   function refreshPage() {
     window.location.reload(false);
   }
 
-  const handleLanguageChange = (id, e) => {
-      console.log("sei la ovalor " + e.target.value)
-      console.log("id é é é: " + id);
-    Axios.put("https://45.79.139.78/v1/management/users/" + id + "/role", {
-        "user_new_data": {
-            "role": ""
-        }
+  function deleteUser(id) {
+    Axios.put("https://45.79.139.78/v1/management/users/" + id + "/delete", {
+      key: "value"
     }, {
-        headers: {
-            Authorization: 'Bearer ' + localStorage.getItem("jwtToken")
-        }
+      headers: {
+          Authorization: 'Bearer ' + localStorage.getItem("jwtToken")
+      }
     }).then(res => {
-        if (res.status === 200) {
-            refreshPage();
-        }
-    }).catch(({ response }) => {
-      setError(response.status);
+      if (res.status === 200) {
+        alert("Usuário deletado do sistema com sucesso");
+        refreshPage();
+      }
+    }).catch(() => {
+      alert("Error in user delete");
     })
-      
   }
-  function modifyRole(id) {
-      console.log("id do usuario" + id);
+
+  function disableUser(id) {
+    Axios.put("https://45.79.139.78/v1/management/users/" + id + "/disable", {
+      key: "value"
+    }, {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem("jwtToken")
+      }
+    }).then(res => {
+      if (res.status === 200) {
+        alert("Usuário desabilitado com sucesso, foi inserido na tela de usuários pendentes novamente");
+        refreshPage();
+      }
+    }).catch(() => {
+      alert("Error in user disable");
+    })
+  }
+
+  function modifyRole(id, value) {
     Axios.put("https://45.79.139.78/v1/management/users/" + id + "/role", {
         "user_new_data": {
-            "role": ""
+            "role": value
         }
     }, {
         headers: {
-            Authorization: 'Bearer ' + localStorage.getItem("jwtToken")
+          Authorization: 'Bearer ' + localStorage.getItem("jwtToken")
         }
     }).then(res => {
         if (res.status === 200) {
+            alert("Role do usuário modificado com sucesso!");
             refreshPage();
         }
-    }).catch(({ response }) => {
-      setError(response.status);
+    }).catch(() => {
+      alert("Error in role modifie");
     })
   }
 
   useEffect(()=>{
     Axios.get("https://45.79.139.78/v1/management/users/active", {
         headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+          Authorization: 'Bearer ' + localStorage.getItem('jwtToken')
         }
     }).then(res => {
         if (res.status === 200) {
             setUsers(res.data.users);
         }
     }).catch(({ response }) => {
-        console.log(response);
+        alert("Error in users active");
     })
     context.selectLang();
   }, [context])
 
   return (<>
   <div>
-    {error ? <p><FormattedMessage id={"erro_request_pending"} /></p> : <></>}
+    {error ? <p><FormattedMessage id={"erro_role"} /></p> : <></>}
     {users.length === 0 ? (
       <p><FormattedMessage id="nenhum_usuario" /></p>
     ) : (
@@ -92,12 +96,18 @@ const App = () => {
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>
-              <select>
+              <select id="select-role" onChange={e => {modifyRole(user.id, e.target.value)}}>
                 <option>{Roles[user.role]}</option>  
                 <option value="1">{Roles[1]}</option>
                 <option value="2">{Roles[2]}</option>
                 <option value="3">{Roles[3]}</option>
               </select>
+              </td>
+              <td>
+                <button onClick={() => disableUser(user.id)}><FormattedMessage id="disable" /></button>
+              </td>
+              <td>
+                <button onClick={() => deleteUser(user.id)}><FormattedMessage id="delete" /></button>
               </td>
             </tr>)}
           </tbody>
--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,31 +1,51 @@
 import React, { useState, useContext, useEffect } from 'react';
 import './sidenav.css';
 import { Context } from '../components/Wrapper';
-
+import Roles from '../utils/roles';
 import { FormattedMessage } from 'react-intl';
+import Axios from 'axios';
 
 const App = () => {
 
   const context = useContext(Context);
 
   const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
 
   function clearLocalStorage(){
     localStorage.removeItem("jwtToken");
     localStorage.removeItem("email");
   }
 
+
+  function getRoleUser() {
+    Axios.get("https://45.79.139.78/v1/auth/user", {
+      headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+      }
+    }).then(res => {
+      if (res.status === 200) {
+          setRole(res.data.user.role);
+      }
+    }).catch(() => {
+    })
+  }
+
   useEffect(()=>{
     context.selectLang();
     setEmail(localStorage.getItem('email'))
+    getRoleUser()
+    context.selectLang();
   }, [context])
 
   return (<>
     <div className="sidebar">
       <div className="Userbox">
         <img alt="avatar" src="https://objetivogru.com/wp-content/uploads/2016/11/fatec.jpg" className="userpic"/>
-        <input type='text' className="username" value={email} disabled={true}></input>
-        <p className="userID">Admin</p>
+        <p>
+          <img alt="" src={require('../img/email_img.png')} width="20px" />   {email}
+        </p>
+        <p>{Roles[role]}</p>
       </div>
       <a href="/home"><FormattedMessage id='inicio' /></a>
       <a href="/import"><FormattedMessage id='importar_dados' /></a>
